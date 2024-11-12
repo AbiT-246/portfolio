86820f5e092d4e5da7e52ed65dd84514b26bd2bf
Abigia Debebe <abigia.debebe@ucalgary.ca>
2024-11-11 19:37:54 -0700

    Version 1

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,34 +1,61 @@
 /*!
 * Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
-* Copyright 2013-2023 Start Bootstrap
+* Copyright 2013-2024 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
 */
-//
-// Scripts
-// 
-
-window.addEventListener('DOMContentLoaded', event => {
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const sideNav = document.body.querySelector('#sideNav');
+window.addEventListener("DOMContentLoaded", (event) => {
+    const sideNav = document.body.querySelector("#sideNav");
     if (sideNav) {
         new bootstrap.ScrollSpy(document.body, {
-            target: '#sideNav',
-            rootMargin: '0px 0px -40%',
+            target: "#sideNav",
+            rootMargin: "0px 0px -40%",
         });
-    };
+    }
 
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const navbarToggler = document.body.querySelector(".navbar-toggler");
     const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
+        document.querySelectorAll("#navbarResponsive .nav-link")
     );
     responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+        responsiveNavItem.addEventListener("click", () => {
+            if (window.getComputedStyle(navbarToggler).display !== "none") {
                 navbarToggler.click();
             }
         });
     });
+});
 
+document.addEventListener("DOMContentLoaded", function () {
+    var typed = new Typed("#typed-text", {
+        strings: ["Abi Debebe", "Programmer", "Poet", "Enthusiast"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true,
+        showCursor: true,
+        cursorChar: "✏️",
+        startDelay: 500,
+        backDelay: 1000,
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const sections = document.querySelectorAll(".fade-in-section");
+
+    const observerOptions = {
+        threshold: 0.3,
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("is-visible");
+            } else {
+                entry.target.classList.remove("is-visible");
+            }
+        });
+    }, observerOptions);
+
+    sections.forEach((section) => {
+        observer.observe(section);
+    });
 });
